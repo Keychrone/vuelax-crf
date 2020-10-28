760ae675d315ab9b37a98f906d8d5d4acc0f465e
Keychrone <106282066+Keychrone@users.noreply.github.com>
2020-10-28 19:22:05 +0000

    fast-export: do not refer to non-existing marks

--- app/src/androidTest/java/com/ssharanyab/coinedone/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ssharanyab/coinedone/ExampleInstrumentedTest.kt
@@ -1,4 +1,6 @@
 package com.ssharanyab.coinedone
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.j
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.T
