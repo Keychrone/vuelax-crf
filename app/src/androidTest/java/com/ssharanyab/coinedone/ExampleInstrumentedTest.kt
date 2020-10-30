22ee3547c3efbbea187ace15cd3129b778c55cbb
Keychrone <106282066+Keychrone@users.noreply.github.com>
2020-10-30 05:34:42 +0000

    Update base.txt

--- app/src/androidTest/java/com/ssharanyab/coinedone/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ssharanyab/coinedone/ExampleInstrumentedTest.kt
@@ -11,4 +11,6 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * See [testing documentation](http://d.android.com
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4
