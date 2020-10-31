f47356a8a597a8dac159dde8822d8690cdcc9941
Keychrone <106282066+Keychrone@users.noreply.github.com>
2020-10-31 08:18:49 +0000

    Remove unused flag

--- app/src/androidTest/java/com/ssharanyab/coinedone/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ssharanyab/coinedone/ExampleInstrumentedTest.kt
@@ -13,4 +13,6 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+   
