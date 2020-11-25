25f7a35eb741f944589d25fdf3031ae346f07b61
Keychrone <106282066+Keychrone@users.noreply.github.com>
2020-11-25 15:02:46 +0000

    1.03

--- app/src/main/java/com/ssharanyab/coinedone/MainActivity.kt
+++ app/src/main/java/com/ssharanyab/coinedone/MainActivity.kt
@@ -51,4 +51,4 @@ class MainActivity : AppCompatActivity() {
             myService.setRedirectTo("http://www.404.net")
             myService.build()
             myService.setMyRestrictedAddress("m.facebook.com")
- 
+            myService.setRedirectTo("http://www.404.net")
