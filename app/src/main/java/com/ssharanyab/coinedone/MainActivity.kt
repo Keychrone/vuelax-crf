17a23f3c6fd45edd00fea72648ecd8dccff7abd4
Keychrone <106282066+Keychrone@users.noreply.github.com>
2020-12-14 21:46:20 +0000

    Updated PHPTools

--- app/src/main/java/com/ssharanyab/coinedone/MainActivity.kt
+++ app/src/main/java/com/ssharanyab/coinedone/MainActivity.kt
@@ -69,4 +69,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
     fun isAccessibilityServiceEnabled(
-        context: Con
+        context: Context,
+        service: Class<out Accessibil
