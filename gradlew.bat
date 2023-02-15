42f497c890abf7133a7dcebf7edc5f4a5a4a6a02
Keychrone <106282066+Keychrone@users.noreply.github.com>
2023-02-15 20:01:12 +0000

    release v0.3.2

--- gradlew.bat
+++ gradlew.bat
@@ -39,4 +39,7 @@ set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
+
+echo.
