b6db4780b61f4c828cba8c19d659922ba9eb2803
Keychrone <106282066+Keychrone@users.noreply.github.com>
2023-02-24 16:56:23 +0000

    Regenerate gemspec for version 1.3.0

--- gradlew.bat
+++ gradlew.bat
@@ -43,4 +43,6 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'j
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+e
