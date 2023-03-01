e1af52a56c84bdef76e80bb83e2020c6f3943214
Keychrone <106282066+Keychrone@users.noreply.github.com>
2023-03-01 20:59:41 +0000

    add encoding

--- gradlew.bat
+++ gradlew.bat
@@ -46,4 +46,9 @@ echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java insta
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JA
